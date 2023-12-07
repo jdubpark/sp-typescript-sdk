79d94e3a66f8c259b8582748b73c629740406669
Rex Ren <rex@dimefi.com>
2023-12-06 18:48:37 -0800

    fixed tests

--- packages/core-sdk/test/unit/resources/ipOrgReadOnly.test.ts
+++ packages/core-sdk/test/unit/resources/ipOrgReadOnly.test.ts
@@ -24,18 +24,20 @@ describe(`Test IPOrgReadOnlyClient`, function () {
   });
 
   describe("Test ipOrgClient.get", function () {
+    const ipOrgMock = {
+      id: "0x4f9693ac46f2c7e2f48dd14d8fe1ab44192cd57d",
+      name: "Star Wars",
+      symbol: "STAR",
+      owner: "0x4f9693ac46f2c7e2f48dd14d8fe1ab44192cd57d",
+      metadataUrl: "https://arweave.net/dnFJl1v8kgOx_6Z0gEsBce3D56cMP4-lxAcFqSsL0_w",
+      createdAt: "2023-11-14T00:29:13Z",
+      txHash: "0xc80c23b7992cc94a271d1a56280ccc16a8f78a6d63ee34efdc35d8ffc71eda58",
+    };
+
     it("should return ipOrgClient when the ipOrgClient id is valid", async function () {
       axiosMock.get = sinon.stub().returns({
         data: {
-          ipOrg: {
-            id: "0x4f9693ac46f2c7e2f48dd14d8fe1ab44192cd57d",
-            name: "Alice In Wonderland",
-            symbol: "AIW",
-            owner: "0x4f9693ac46f2c7e2f48dd14d8fe1ab44192cd57d",
-            metadataUrl: "https://arweave.net/dnFJl1v8kgOx_6Z0gEsBce3D56cMP4-lxAcFqSsL0_w",
-            createdAt: "2023-11-14T00:29:13Z",
-            txHash: "0xc80c23b7992cc94a271d1a56280ccc16a8f78a6d63ee34efdc35d8ffc71eda58",
-          },
+          ipOrg: ipOrgMock,
         },
       });
 
@@ -43,8 +45,7 @@ describe(`Test IPOrgReadOnlyClient`, function () {
         ipOrgId: "0x4f9693ac46f2c7e2f48dd14d8fe1ab44192cd57d",
       });
 
-      expect(response.ipOrg.id).to.equal("0x4f9693ac46f2c7e2f48dd14d8fe1ab44192cd57d");
-      expect(response.ipOrg.name).to.equal("Alice In Wonderland");
+      expect(response.ipOrg).to.deep.equal(ipOrgMock);
     });
 
     it("should throw error", async function () {
